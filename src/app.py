1e1cc4ab9904f59b7986b5a931f8fa199274ce37
arossi <alfredoc.rossi@gmail.com>
2021-07-10 19:18:27 -0300

    nueva prueba

--- src/app.py
+++ src/app.py
@@ -1,17 +1,9 @@
 from flask import Flask
-from flask import render_template
-from flaskext.mysql import MySQL
-
 app = Flask(__name__)
-mysql = MySQL()
 
-app.config['MySQL_DATABASE_HOST'] = 'localhost'
-app.config['MySQL_DATABASE_USER'] = 'root'
-app.config['MySQL_DATABASE_PASSWORD'] = 'tano1234' 
-app.config['MySQL_DATABASE_DB'] = 'empleados'
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-mysql.init_app(app)
-
-
-if __name__ == '__main__':
-    app.run(debu=True)
+if __name__ == "__main__":
+    app.run()
